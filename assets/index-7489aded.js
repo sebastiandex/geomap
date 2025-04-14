21b8f5c76e88b475bec749a0176ecda3e2b58e2c
Andrey Sebastianov <a.sebastianov@z-it.ru>
2025-04-14 22:37:00 +1000

    Updates

---
+++ assets/index-7489aded.js
@@ -0,0 +1,1 @@
+import{d as r,c as e,b as o}from"./index-3cb7d34b.js";const t={class:"ErrorView"},_=r({__name:"ErrorView",setup(s){return(c,n)=>(o(),e("div",t,"Error Page"))}});export{_ as default};
