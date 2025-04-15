ba5852f08f4d589bd07614a1cc775be170e7251f
Andrey Sebastianov <a.sebastianov@z-it.ru>
2025-04-15 14:47:22 +1000

    Добавлена цветовая индикация из точки отправления по типу сообщения

--- src/entities/telegram/lib/renderTelegram.ts
+++ src/entities/telegram/lib/renderTelegram.ts
@@ -28,7 +28,7 @@ export const renderTelegram: RenderFunction = (_coords, state) => {
   ctx.closePath()
   // Render Circle
   ctx.beginPath()
-  ctx.arc(startX, startY, 1 * ratio, 0, 2 * Math.PI)
+  ctx.arc(startX, startY, 2 * ratio, 0, 2 * Math.PI)
   // ctx.fillStyle = theme === 'dark' ? '#626364' : '#fefefe'
   ctx.fillStyle = getTelegramColor(store.active[0].type)
   ctx.fill()
