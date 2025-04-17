e5de8b503ca2e989e0d9e7833ea197c99f0b2e68
Andrey Sebastianov <a.sebastianov@z-it.ru>
2025-04-17 17:31:54 +1000

    Updates

--- assets/index-49b64899.js
+++
@@ -1,1 +0,0 @@
-import{d as r,c as e,b as o}from"./index-d15fc4a1.js";const t={class:"ErrorView"},_=r({__name:"ErrorView",setup(s){return(c,n)=>(o(),e("div",t,"Error Page"))}});export{_ as default};
